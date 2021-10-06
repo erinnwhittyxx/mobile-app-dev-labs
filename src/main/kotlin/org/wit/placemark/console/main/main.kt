a150e900793731f16f25fb2c032f5ed3c073ccd9
erinnwhittyxx <erinnwhittyxx@gmail.com>
2021-10-06 20:32:19 +0100

    first commit

---
+++ src/main/kotlin/org/wit/placemark/console/main/main.kt
@@ -0,0 +1,63 @@
+package org.wit.placemark.console.main
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+fun main(args: Array<String>) {
+    logger.info { "Launching Placemark Console App" }
+    println("Placemark Kotlin App Version 1.0")
+
+    var input: Int
+
+    do {
+        input = menu()
+        when(input) {
+            1 -> addPlacemark()
+            2 -> updatePlacemark()
+            3 -> listPlacemarks()
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
+    logger.info { "Shutting Down Placemark Console App" }
+}
+
+fun menu() : Int {
+
+    var option : Int
+    var input: String? = null
+
+    println("MAIN MENU")
+    println(" 1. Add Placemark")
+    println(" 2. Update Placemark")
+    println(" 3. List All Placemarks")
+    println("-1. Exit")
+    println()
+    print("Enter an integer : ")
+    input = readLine()!!
+    option = if (input.toIntOrNull() != null && !input.isEmpty())
+        input.toInt()
+    else
+        -9
+    return option
+}
+
+fun addPlacemark(){
+    var title : String
+
+    println("Add Placemark")
+    println()
+    print("Enter a Title : ")
+    title = readLine()!!
+    println("You entered $title for title")
+}
+
+fun updatePlacemark() {
+    println("You Chose Update Placemark")
+}
+
+fun listPlacemarks() {
+    println("You Chose List All Placemarks")
+}
